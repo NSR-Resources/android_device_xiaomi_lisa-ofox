1f141666653679aab516ae65a61a8e7a27733da3
NSR-XINE <nasironfire07@gmail.com>
2026-02-09 14:09:29 +0400

    lisa: add health hals

--- device.mk
+++ device.mk
@@ -62,3 +62,8 @@ PRODUCT_PACKAGES += \
     update_engine \
     update_engine_sideload \
     update_verifier
+
+#  Health
+PRODUCT_PACKAGES += \
+    android.hardware.health@2.1-impl.recovery \
+    android.hardware.health@2.1-service
