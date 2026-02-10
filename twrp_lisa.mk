774493876f813596ed55f9cbb5328bb156405914
NSR-XINE <nasironfire07@gmail.com>
2026-02-10 19:44:44 +0100

    lisa: initial ofox changes

--- twrp_lisa.mk
+++ twrp_lisa.mk
@@ -20,7 +20,10 @@ $(call inherit-product, vendor/twrp/config/common.mk)
 # Inherit any TWRP-specific settings
 $(call inherit-product-if-exists, $(DEVICE_PATH)/twrp_config.mk)
 
-## Device identifier. This must come after all inclusions
+# Inherit any OrangeFox-specific settings
+$(call inherit-product-if-exists, $(DEVICE_PATH)/fox_lisa.mk)
+
+# Device identifier. This must come after all inclusions
 PRODUCT_DEVICE := $(PRODUCT_RELEASE_NAME)
 PRODUCT_NAME := twrp_$(PRODUCT_RELEASE_NAME)
 PRODUCT_BRAND := xiaomi
